75ba79172ad2bc67b558f32692b3e2ce5e4cb7a7
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-03-08 21:06:52 +0100

    updated to Groovy 4, included as workaround
    * Spock tests require a "requires" in the module-info,
      which in turn requires the implementation dependency
    * see https://github.com/spockframework/spock/issues/1227

--- fxApp/src/main/java/module-info.java
+++ fxApp/src/main/java/module-info.java
@@ -1,10 +1,14 @@
 module org.hansib.javaFxApp {
-	requires org.hansib.javaFxApp.utilities;
-	requires org.apache.logging.log4j;
+
+	exports javafx_app.app;
 
 	requires transitive javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
 
-	exports javafx_app.app;
+	requires org.apache.logging.log4j;
+
+	// required for Spock tests
+	// see https://github.com/spockframework/spock/issues/1227
+	requires org.apache.groovy;
 }
